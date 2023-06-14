13f0f8119f531d2b197ad59c8488e627fedc0d33
santiTectonica <santiago@tectonica.co>
2023-06-14 16:02:56 -0300

    everyaction test

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,19 +18,46 @@ const styles = StyleSheet.create({
 const IndexPage = () => {
   return (
     <main>
-      
-      <PDFViewer>
-          <Pdf />
-      </PDFViewer>
-      
-      
-      <PDFDownloadLink document={<Pdf />}>Descargar </PDFDownloadLink>
-      
-      
+      {
+      /*
+        <PDFViewer>
+            <Pdf />
+        </PDFViewer>
+
+        <PDFDownloadLink document={<Pdf />}>Descargar </PDFDownloadLink>
+      */
+      }
+      <div class="ngp-form"
+
+        data-form-url="https://secure.everyaction.com/v1/Forms/0sXNvwKAtEaTn2D98vZ3Ng2"
+
+        data-fastaction-endpoint="https://fastaction.ngpvan.com"
+
+        data-inline-errors="true"
+
+        data-fastaction-nologin="true"
+
+        data-databag-endpoint="https://profile.ngpvan.com"
+
+        data-databag="everybody"
+
+        data-mobile-autofocus="false">
+
+      </div>
+
     </main>
   )
 }
 
 export default IndexPage
 
-export const Head = () => <title>Home Page</title>
+/*export const Head = () => <title>Home Page</title> */
+
+export const Head = () => (
+  <>
+    <title>Home Page</title>
+    <link rel='preload' href='https://static.everyaction.com/ea-actiontag/at.js' as='script' crossorigin='anonymous' />
+    <link rel='preload' href='https://static.everyaction.com/ea-actiontag/at.min.css' as='style' />
+    <script type='text/javascript' src='https://static.everyaction.com/ea-actiontag/at.js' crossorigin='anonymous'></script>
+  </>
+)
